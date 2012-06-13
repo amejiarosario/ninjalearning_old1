d32e82ca3a3119275c856b70bc151fc2de458ff6
Adrian Mejia <axm7988@rit.edu>
2012-06-12 22:45:32 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ninjalearning::Application.config.secret_token = '78c8d0f4eb433fc37aab90c7fb4ba87041b7f45683177e2d74c7822e5c56c3a813016d0aa40b08ac29794e3829b8115fc8063485d427f044221e220c2ac967f6'
